68b4c1fa0f00123a3c763a2d7fdcd584793296aa
KenOtis <66284239+KenOtis@users.noreply.github.com>
2021-07-30 22:48:17 -0400

    Add files via upload

---
+++ ATBSLesson5.py
@@ -0,0 +1,25 @@
+#if else statements 
+
+#If statement
+name = 'alice' 
+if name == 'alice':
+    print ('Hello Alice')
+print ('Done')
+
+#If else Statement--> Password
+password='Pencil'
+if password=='Pencil':
+    print("Acess Granted")
+else:
+    print("Wrong Password")
+
+name='Kenny'
+age= 21
+if name=='Alice':
+    print('Hello Alice')
+elif age<12:
+    print("You are not Alice you child!")
+elif age>200:
+    print("Unlike you, Alice is not a wacky old person")
+elif age ==21:
+    print("Hello, Alice?")
