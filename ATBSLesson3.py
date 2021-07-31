ac3ee6f88aaa350d00503a3a9214c49b622c34ad
KenOtis <66284239+KenOtis@users.noreply.github.com>
2021-07-30 22:48:06 -0400

    Add files via upload

---
+++ ATBSLesson3.py
@@ -0,0 +1,11 @@
+#Automate the Boring stuff
+#This program sayas hello and asks for your name 
+print ("Hello World!")
+print('What is your name? ')
+myName=input()
+print('It is nice to meet you, ',myName)
+print("The Length of your name is: ")
+print(len(myName))
+print('What is your age?' )
+myAge=input()
+print('You will be ', str(int(myAge)+1), "in 1 year")
